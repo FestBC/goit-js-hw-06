c16d08a604458d8d1d8f6e9acf380f33ce8056c9
Fest <brychkapovozka@gmail.com>
2025-05-09 03:27:19 +0200

    Complete HW.

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,38 @@
 "use strict";
 
+class StringBuilder {
+  #value;
 
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
 
 console.log("//Start of task 3.");
 
-
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
 
 console.log("//End of task 3.");
